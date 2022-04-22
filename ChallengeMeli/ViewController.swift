3f2e5a25655cb01603bf194435533082669388d3
Carlos Tijerino <64485611+carlosTijerino@users.noreply.github.com>
2022-04-21 22:53:08 -0500

    first commit

--- ChallengeMeli/ViewController.swift
+++ ChallengeMeli/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Hola")
+        
         
         let delayTime = DispatchTime.now() + 3.0
           
